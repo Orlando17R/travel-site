d859cf9eec06761c44ae19ff26037b9933c8f4de
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-07-07 16:39:28 -0400

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,45 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+/*************VARIABLES****************/
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+/*************************************/
+/*************************************/
+
+
+
+/***************TASK*******************/
 gulp.task('default',function()
 {
     console.log("gulp task");
-}
-);
-
+});
+/*************************************/
 gulp.task('html', function()
 {
     console.log("HTML task");
-}
-);
-
+});
+/*************************************/
+gulp.task('styles', function()
+{
+    /*console.log("CSS task");*/
+    return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssvars,nested,autoprefixer]) )
+    .pipe(gulp.dest('./app/temp.styles'));
+});
+/*************************************/
 gulp.task('watch',function()
 {
     watch('./app/index.html',function()
     {
         gulp.start('html');
     } );
-}
-);
+
+    watch('./app/assets/styles/**/*.css',function()
+    {
+        gulp.start('styles');
+    });
+
+});
+/*************************************/

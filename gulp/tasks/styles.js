aa2d8bc24c53e12765531f35d351600d5ef7a18a
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-07-08 15:34:30 -0400

    mas cambios

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,25 @@
+/*************VARIABLES****************/
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins') ;
+/*************************************/
+
+
+/*****************TASK****************/
+gulp.task('styles', function()
+{
+    /*console.log("CSS task");*/
+    return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer]) )
+    .on('error', function(errorInfo)
+    {
+        console.log(errorInfo.toString() );
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp.styles'));
+});
+/*************************************/
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,21 @@
-/*************VARIABLES****************/
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+/*************VARIABLES***************
 var gulp = require('gulp'),
-watch = require('gulp-watch');
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
-/*************************************/
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 /*************************************/
 
-
-
-/***************TASK*******************/
+/***************TASK*******************
 gulp.task('default',function()
 {
     console.log("gulp task");
 });
-/*************************************/
+/*************************************
 gulp.task('html', function()
 {
     console.log("HTML task");
-});
-/*************************************/
-gulp.task('styles', function()
-{
-    /*console.log("CSS task");*/
-    return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport,cssvars,nested,autoprefixer]) )
-    .pipe(gulp.dest('./app/temp.styles'));
-});
-/*************************************/
-gulp.task('watch',function()
-{
-    watch('./app/index.html',function()
-    {
-        gulp.start('html');
-    } );
-
-    watch('./app/assets/styles/**/*.css',function()
-    {
-        gulp.start('styles');
-    });
-
 });
 /*************************************/

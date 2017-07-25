28fbce4414a6b4f83a1985798d0e7870d854f2db
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-07-25 16:32:00 -0400

    complete sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,16 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+
 
 var mobileMenu = new MobileMenu();
-//var revealOnScroll = new RevealOnScroll();
 
+//var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"),'85%');
 new RevealOnScroll($(".testimonials"),'60%');
+
+var stickyHeader = new StickyHeader();
 
 /*var $ = require('jquery');
 //var Person = require('./modules/Person');

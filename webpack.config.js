ee9cb62ed9f033758a722cf98671519896f2e8dc
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-07-26 00:28:22 -0400

    lazyloading simple complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 //var webpack = require("webpack");
+const path = require('path');
+
 module.exports = {
-    entry: __dirname + "/app/assets/scripts/App.js",
+    entry: /*__dirname + */{ 
+        'App':__dirname + '/app/assets/scripts/App.js',
+        'Vendor':__dirname + '/app/assets/scripts/Vendor.js'
+    },
     output:
         {
-            path: __dirname + "/app/temp.styles/scripts",
-            filename: "App.js"
+            path:__dirname + "/app/temp.styles/scripts",
+            filename: "[name].js"
         },
 
     module:

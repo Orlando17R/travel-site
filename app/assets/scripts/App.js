73e6898565462f9f1cb357c0bb5b955e90c56e81
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-07-25 23:21:43 -0400

    modal complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 var mobileMenu = new MobileMenu();
@@ -11,6 +12,11 @@ new RevealOnScroll($(".feature-item"),'85%');
 new RevealOnScroll($(".testimonials"),'60%');
 
 var stickyHeader = new StickyHeader();
+
+
+var modal = new Modal();
+
+
 
 /*var $ = require('jquery');
 //var Person = require('./modules/Person');

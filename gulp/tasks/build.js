56cbf6a4314e0175ee60509571abcc78f2e14c83
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-08-02 10:46:50 -0400

    complete gulp build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,12 +1,30 @@
+/**************VARIABLES******/
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano= require('gulp-cssnano'),
+uglify= require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
 /***************************************/
-gulp.task('deleteDistFolder', function()
+gulp.task('previewDist', function() 
 {
-    return del("./dist");
+    browserSync.init(
+        {
+            notify:false,
+            server: 
+            {
+                baseDir: "docs"
+            }
+        }
+    );
+});
+/***************************************/
+gulp.task('deleteDistFolder',['icons'], function()
+{
+    return del("./docs");
 });
 
 /***************************************/
@@ -19,16 +37,28 @@ gulp.task('optimizeImages',['deleteDistFolder'],function()
             interlaced: true,
             multipass:true
         }) )
-    .pipe( gulp.dest('./dist/assets/images') );
+    .pipe( gulp.dest('./docs/assets/images') );
 });
 
 /***************************************/
-gulp.task('usemin',['deleteDistFolder'], function()
+gulp.task('useminTrigger',['deleteDistFolder'],function()
+{
+    gulp.start("usemin");
+});
+
+
+/***************************************/
+gulp.task('usemin',['styles','scripts'], function()
 {
         return gulp.src("./app/index.html")
-        .pipe(usemin() )
-        .pipe(gulp.dest("./dist") );
+        .pipe(usemin(
+            {
+                css:[function () {return rev() }, function() {return cssnano()} ],
+                js:[function() {return rev() }, function() {return uglify()} ]
+            }
+        ) )
+        .pipe(gulp.dest("./docs") );
 });
 
 /***************************************/
-gulp.task('build',['deleteDistFolder','optimizeImages','usemin']);
+gulp.task('build',['deleteDistFolder','optimizeImages','useminTrigger']);
